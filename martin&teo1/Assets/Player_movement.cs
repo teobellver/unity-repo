c08c00e646218624598432dd7323175ac8623888
teobellver <85182749+teobellver@users.noreply.github.com>
2023-05-21 16:47:54 -0300

    key

--- martin&teo1/Assets/Player_movement.cs
+++ martin&teo1/Assets/Player_movement.cs
@@ -13,12 +13,13 @@ public class Player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Izquierda derecha
-        if (Input.Getkey("a")
+        //mover player Izquierda 
+        if (Input.GetKey("a"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1000f * Time.deltaTime, 0));
         }
-        if (Input.Getkey("d")
+        //mover player Derecha
+        if (Input.GetKey("d"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1000f * Time.deltaTime, 0));
         }
