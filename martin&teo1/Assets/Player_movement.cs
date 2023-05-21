2568e72e6122581dde4a77b73c91ea563353abe9
teobellver <85182749+teobellver@users.noreply.github.com>
2023-05-21 17:05:51 -0300

    jump

--- martin&teo1/Assets/Player_movement.cs
+++ martin&teo1/Assets/Player_movement.cs
@@ -7,33 +7,23 @@ public class Player_movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< HEAD
-=======
-        
-    gameObject.transform.position = new Vector3(-70, 20, 0);
-    }
->>>>>>> 12c27d2778d4e0c93b60ddaf863ae600bff04820
-
+        gameObject.transform.position = new Vector3(-70, 20, 0);
     }
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         //Izquierda derecha
-=======
-        //mover player Izquierda 
->>>>>>> 12c27d2778d4e0c93b60ddaf863ae600bff04820
         if (Input.GetKey("a"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10000f * Time.deltaTime, 0));
         }
-<<<<<<< HEAD
-=======
-        //mover player Derecha
->>>>>>> 12c27d2778d4e0c93b60ddaf863ae600bff04820
         if (Input.GetKey("d"))
         {
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(10000f * Time.deltaTime, 0));
+        }
+        //jump 
+        if (Input.GetKeyDown("space")){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5000f));
         }
         //salto
     }
