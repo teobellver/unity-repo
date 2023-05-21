12c27d2778d4e0c93b60ddaf863ae600bff04820
teobellver <85182749+teobellver@users.noreply.github.com>
2023-05-21 16:59:47 -0300

    player

--- martin&teo1/Assets/Player_movement.cs
+++ martin&teo1/Assets/Player_movement.cs
@@ -7,7 +7,8 @@ public class Player_movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.transform.position = new Vector3(-70,40,0);
+        
+    gameObject.transform.position = new Vector3(-70, 20, 0);
     }
 
     // Update is called once per frame
