756b627fadbd1c527afb89045bd6c974e7ccbeb2
teobellver <85182749+teobellver@users.noreply.github.com>
2023-05-21 19:40:22 -0300

    jump if [...] && position y <= 0

--- martin&teo1/Assets/Player_movement.cs
+++ martin&teo1/Assets/Player_movement.cs
@@ -25,7 +25,7 @@ public class Player_movement : MonoBehaviour
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(12000f * Time.deltaTime, 0));
         }
         //jump 
-        if (Input.GetKeyDown("space") && canJump == true){
+        if (Input.GetKeyDown("space") && canJump && gameObject.transform.position.y>=0){
             gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 7000f));
             canJump = false;
         }
