13a3a5ddf14de5e576eef4435a82dbb5bf7a2afc
teobellver <85182749+teobellver@users.noreply.github.com>
2023-05-21 16:29:06 -0300

    start pos

--- martin&teo1/Assets/gravity.cs
+++ martin&teo1/Assets/gravity.cs
@@ -7,12 +7,12 @@ public class gravity : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        gameObject.transform.position = new Vector3(-10,2,0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = new Vector3(-10,2,0);
         
     }
 }
